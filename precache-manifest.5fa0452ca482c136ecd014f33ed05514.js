62982a2a4134be33d662b4190c6c7f8773f5bd2e
dubbsong <dubbsong@gmail.com>
2019-07-07 20:19:14 +0900

    Updates

---
+++ precache-manifest.5fa0452ca482c136ecd014f33ed05514.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a48b8f5890b999756040039996df34",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "9697dacce97df664e3b9",
+    "url": "/test_app/static/css/main.126fc194.chunk.css"
+  },
+  {
+    "revision": "ccb29facb9d65a224534",
+    "url": "/test_app/static/js/2.8af17d14.chunk.js"
+  },
+  {
+    "revision": "9697dacce97df664e3b9",
+    "url": "/test_app/static/js/main.589cbc4b.chunk.js"
+  },
+  {
+    "revision": "761d5a24e181ea8aa337",
+    "url": "/test_app/static/js/runtime~main.47a54dbf.js"
+  }
+]);
